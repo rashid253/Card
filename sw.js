46b7fccd898d05adcb893862157f1e8ea58cd0fb
rashid253 <rashidncit@gmail.com>
2026-02-02 15:15:13 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,35 @@
 const CACHE = "fsd-directory-v1";
 
 const FILES = [
-  "./index.html",
-  "./create.html",
-  "./card.html",
-  "./edit.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+ "./index.html",
+ "./create.html",
+ "./card.html",
+ "./manifest.json",
+ "./icon-192.png",
+ "./icon-512.png"
 ];
 
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+ e.waitUntil(
+  caches.open(CACHE).then(c => c.addAll(FILES))
+ );
 });
 
 self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+ e.waitUntil(
+  caches.keys().then(keys =>
+   Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  )
+ );
 });
 
+// ❗ Supabase requests are NOT cached
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+ if(e.request.url.includes("supabase")){
+  return;
+ }
+
+ e.respondWith(
+  caches.match(e.request).then(r => r || fetch(e.request))
+ );
 });
